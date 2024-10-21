7588d8a9366734feb7675e6210085f59cd1f3670
divy2545 <divyanshh2545@gmail.com>
2024-10-21 10:21:55 +0530

    first commit

---
+++ Blogs/models/blog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// creating mongoose
+
+let blogSchema = mongoose.Schema({
+     title:{
+            type : String,
+            trim : true,
+            required : true
+     },
+     
+     author:{
+            type : String,
+            trim : true,
+            required : true
+     },
+      comment:{
+            type : String,
+            trim : true,
+            required : true
+     }
+
+})
+
+
+let Blog = mongoose.model('Blog',blogSchema)
+
+module.exports = Blog;
